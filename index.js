9eef89091cc6b0ae28327cb663e28aa24c38013b
milliorn <scottmilliorn@gmail.com>
2022-12-14 00:25:59 -0800

    comments

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// https://www.npmjs.com/package/dotenv
 const dotenv = require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
+// https://expressjs.com/en/5x/api.html#app.use
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 // middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/openai", require("./routes/openAiRoutes"));
-
+// https://expressjs.com/en/5x/api.html#app.listen
 app.listen(port, () => {
   console.log(`Server running http://localhost:${port}`);
 });
